004d153339ce096006d4eb68e9088760157b3a68
sanket-sigmoid <105432861+sanket-sigmoid@users.noreply.github.com>
2022-06-10 14:06:22 +0530

    Update test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -3,6 +3,7 @@ import main
 
 def test_add():
     assert main.add(3, 4) == 7
+    assert main.add(3, 4) == 7
     assert main.add(3.5, 4) == 7
     assert main.add(3.9, 4) == 7
     assert main.add(3.9, 4.1) == 8
